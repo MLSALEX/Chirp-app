80a8630390984009cc7ce263ecc49155378136f9
MLSALEX <stiopovna@mail.ru>
2026-01-03 11:03:24 +0200

    Initial commit

---
+++ composeApp/src/jvmMain/kotlin/com/alexmls/chirp/main.kt
@@ -0,0 +1,13 @@
+package com.alexmls.chirp
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Chirp",
+    ) {
+        App()
+    }
+}
