dade66fc6715c33a49d8490a384e409f8057739d
MLSALEX <stiopovna@mail.ru>
2026-01-07 13:38:47 +0200

    Implement HttpClientFactory

---
+++ core/domain/src/commonMain/kotlin/com/alexmls/core/domain/logging/ChirpLogger.kt
@@ -0,0 +1,9 @@
+package com.alexmls.core.domain.logging
+
+
+interface ChirpLogger {
+    fun debug(message: String)
+    fun info(message: String)
+    fun warn(message: String)
+    fun error(message: String, throwable: Throwable? = null)
+}
