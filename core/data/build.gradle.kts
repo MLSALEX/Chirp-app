47bbf76c4facd559bdf9fdeb2907a5e5e24f89cd
MLSALEX <stiopovna@mail.ru>
2026-01-07 12:38:14 +0200

    Handle HTTP client results

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -15,6 +15,8 @@ kotlin {
                 implementation(libs.kotlin.stdlib)
 
                 implementation(projects.core.domain)
+
+                implementation(libs.bundles.ktor.common)
             }
         }
 
@@ -23,6 +25,7 @@ kotlin {
                 // Add Android-specific dependencies here. Note that this source set depends on
                 // commonMain by default and will correctly pull the Android artifacts of any KMP
                 // dependencies declared in commonMain.
+                implementation(libs.ktor.client.okhttp)
             }
         }
 
@@ -33,6 +36,7 @@ kotlin {
                 // part of KMP’s default source set hierarchy. Note that this source set depends
                 // on common by default and will correctly pull the iOS artifacts of any
                 // KMP dependencies declared in commonMain.
+                implementation(libs.ktor.client.darwin)
             }
         }
     }
