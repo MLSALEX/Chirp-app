706348a42b7a32de2fe4451aad4219578435ed8d
MLSALEX <stiopovna@mail.ru>
2026-01-10 10:51:03 +0200

    Implement register API endpoint

---
+++ core/data/src/commonMain/kotlin/com/alexmls/core/data/auth/KtorAuthService.kt
@@ -0,0 +1,28 @@
+package com.alexmls.core.data.auth
+
+import com.alexmls.core.data.dto.requests.RegisterRequest
+import com.alexmls.core.data.networking.post
+import com.alexmls.core.domain.auth.AuthService
+import com.alexmls.core.domain.util.DataError
+import com.alexmls.core.domain.util.EmptyResult
+import io.ktor.client.HttpClient
+
+class KtorAuthService(
+    private val httpClient: HttpClient
+): AuthService {
+
+    override suspend fun register(
+        email: String,
+        username: String,
+        password: String
+    ): EmptyResult<DataError.Remote> {
+        return httpClient.post(
+            route = "/auth/register",
+            body = RegisterRequest(
+                email = email,
+                username = username,
+                password = password
+            )
+        )
+    }
+}
---
+++ core/domain/src/commonMain/kotlin/com/alexmls/core/domain/auth/AuthService.kt
@@ -0,0 +1,12 @@
+package com.alexmls.core.domain.auth
+
+import com.alexmls.core.domain.util.DataError
+import com.alexmls.core.domain.util.EmptyResult
+
+interface AuthService {
+    suspend fun register(
+        email: String,
+        username: String,
+        password: String
+    ): EmptyResult<DataError.Remote>
+}
