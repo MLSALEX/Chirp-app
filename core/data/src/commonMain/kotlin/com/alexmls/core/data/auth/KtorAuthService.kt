bad27f2fdce3636097ac1c4483a1d54d561f239a
MLSALEX <stiopovna@mail.ru>
2026-01-15 10:49:02 +0200

    Implement reset password functionality

--- core/data/src/commonMain/kotlin/com/alexmls/core/data/auth/KtorAuthService.kt
+++ core/data/src/commonMain/kotlin/com/alexmls/core/data/auth/KtorAuthService.kt
@@ -4,6 +4,7 @@ import com.alexmls.core.data.dto.AuthInfoSerializable
 import com.alexmls.core.data.dto.requests.EmailRequest
 import com.alexmls.core.data.dto.requests.LoginRequest
 import com.alexmls.core.data.dto.requests.RegisterRequest
+import com.alexmls.core.data.dto.requests.ResetPasswordRequest
 import com.alexmls.core.data.mappers.toDomain
 import com.alexmls.core.data.networking.get
 import com.alexmls.core.data.networking.post
@@ -69,6 +70,19 @@ class KtorAuthService(
         return httpClient.post<EmailRequest, Unit>(
             route = "/auth/forgot-password",
             body = EmailRequest(email)
+        )
+    }
+
+    override suspend fun resetPassword(
+        newPassword: String,
+        token: String
+    ): EmptyResult<DataError.Remote> {
+        return httpClient.post(
+            route = "/auth/reset-password",
+            body = ResetPasswordRequest(
+                newPassword = newPassword,
+                token = token
+            )
         )
     }
 }
