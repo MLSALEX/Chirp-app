a2aa2173b3325dac6b55bb88030e8fa5de514c1d
MLSALEX <stiopovna@mail.ru>
2026-01-22 09:24:18 +0200

    Implement chat list screen UI

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/alexmls/chat/presentation/model/MessageUi.kt
@@ -0,0 +1,27 @@
+package com.alexmls.chat.presentation.model
+
+import com.alexmls.chat.domain.models.ChatMessageDeliveryStatus
+import com.alexmls.core.designsystem.components.avatar.ChatParticipantUi
+import com.alexmls.core.presentation.util.UiText
+
+sealed interface MessageUi {
+    data class LocalUserMessage(
+        val id: String,
+        val content: String,
+        val deliveryStatus: ChatMessageDeliveryStatus,
+        val isMenuOpen: Boolean,
+        val formattedSentTime: UiText
+    ) : MessageUi
+
+    data class OtherUserMessage(
+        val id: String,
+        val content: String,
+        val formattedSentTime: UiText,
+        val sender: ChatParticipantUi
+    ) : MessageUi
+
+    data class DateSeparator(
+        val id: String,
+        val date: UiText,
+    ) : MessageUi
+}
