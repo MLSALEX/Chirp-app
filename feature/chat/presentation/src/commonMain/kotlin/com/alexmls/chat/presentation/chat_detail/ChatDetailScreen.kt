2a420ef4108c9347b20c72b353af20dc8fc32148
MLSALEX <stiopovna@mail.ru>
2026-02-02 10:29:15 +0200

    Maintain the message date banner & auto scroll to bottom when new messages arrive

--- feature/chat/presentation/src/commonMain/kotlin/com/alexmls/chat/presentation/chat_detail/ChatDetailScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/alexmls/chat/presentation/chat_detail/ChatDetailScreen.kt
@@ -1,6 +1,8 @@
 package com.alexmls.chat.presentation.chat_detail
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.rememberTextFieldState
@@ -20,8 +23,11 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -29,6 +35,8 @@ import androidx.compose.ui.backhandler.BackHandler
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import chirp.feature.chat.presentation.generated.resources.Res
@@ -37,6 +45,8 @@ import chirp.feature.chat.presentation.generated.resources.select_a_chat
 import com.alexmls.chat.domain.models.ChatMessage
 import com.alexmls.chat.domain.models.ChatMessageDeliveryStatus
 import com.alexmls.chat.presentation.chat_detail.components.ChatDetailHeader
+import com.alexmls.chat.presentation.chat_detail.components.DateChip
+import com.alexmls.chat.presentation.chat_detail.components.MessageBannerListener
 import com.alexmls.chat.presentation.chat_detail.components.MessageBox
 import com.alexmls.chat.presentation.chat_detail.components.MessageList
 import com.alexmls.chat.presentation.chat_detail.components.PaginationScrollListener
@@ -52,6 +62,7 @@ import com.alexmls.core.presentation.util.UiText
 import com.alexmls.core.presentation.util.clearFocusOnTap
 import com.alexmls.core.presentation.util.currentDeviceConfiguration
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -72,11 +83,16 @@ fun ChatDetailRoot(
     val state by viewModel.state.collectAsStateWithLifecycle()
 
     val snackbarState = remember { SnackbarHostState() }
+    val messageListState = rememberLazyListState()
+    val scope = rememberCoroutineScope()
+
     ObserveAsEvents(viewModel.events) { event ->
         when (event) {
             ChatDetailEvent.OnChatLeft -> onBack()
             ChatDetailEvent.OnNewMessage -> {
-                // TODO: Auto scroll to bottom
+                scope.launch {
+                    messageListState.animateScrollToItem(0)
+                }
             }
             is ChatDetailEvent.OnError -> {
                 snackbarState.showSnackbar(event.error.asStringAsync())
@@ -88,7 +104,6 @@ fun ChatDetailRoot(
         viewModel.onAction(ChatDetailAction.OnSelectChat(chatId))
     }
 
-    val scope = rememberCoroutineScope()
     BackHandler(
         enabled = !isDetailPresent
     ) {
@@ -103,10 +118,12 @@ fun ChatDetailRoot(
 
     ChatDetailScreen(
         state = state,
+        messageListState = messageListState,
         isDetailPresent = isDetailPresent,
         onAction = { action ->
             when (action) {
                 is ChatDetailAction.OnChatMembersClick -> onChatMembersClick()
+                is ChatDetailAction.OnBackClick -> onBack()
                 else -> Unit
             }
             viewModel.onAction(action)
@@ -118,12 +135,12 @@ fun ChatDetailRoot(
 @Composable
 fun ChatDetailScreen(
     state: ChatDetailState,
+    messageListState: LazyListState,
     isDetailPresent: Boolean,
     snackbarState: SnackbarHostState,
     onAction: (ChatDetailAction) -> Unit,
 ) {
     val configuration = currentDeviceConfiguration()
-    val messageListState = rememberLazyListState()
 
     val realMessageItemCount = remember(state.messages) {
         state
@@ -131,6 +148,27 @@ fun ChatDetailScreen(
             .filter { it is MessageUi.LocalUserMessage || it is MessageUi.OtherUserMessage }
             .size
     }
+    LaunchedEffect(messageListState) {
+        snapshotFlow {
+            messageListState.firstVisibleItemIndex to messageListState.layoutInfo.totalItemsCount
+        }.filter { (firstVisibleIndex, totalItemsCount) ->
+            firstVisibleIndex >= 0 && totalItemsCount > 0
+        }.collect { (firstVisibleItemIndex, _) ->
+            onAction(ChatDetailAction.OnFirstVisibleIndexChanged(firstVisibleItemIndex))
+        }
+    }
+
+    MessageBannerListener(
+        lazyListState = messageListState,
+        messages = state.messages,
+        isBannerVisible = state.bannerState.isVisible,
+        onShowBanner = { index ->
+            onAction(ChatDetailAction.OnTopVisibleIndexChanged(index))
+        },
+        onHide = {
+            onAction(ChatDetailAction.OnHideBanner)
+        }
+    )
 
     PaginationScrollListener(
         lazyListState = messageListState,
@@ -141,6 +179,11 @@ fun ChatDetailScreen(
             onAction(ChatDetailAction.OnScrollToTop)
         }
     )
+
+    var headerHeight by remember {
+        mutableStateOf(0.dp)
+    }
+    val density = LocalDensity.current
 
     Scaffold(
         modifier = Modifier
@@ -181,7 +224,14 @@ fun ChatDetailScreen(
                                 .fillMaxSize()
                         )
                     }else {
-                        ChatHeader {
+                        ChatHeader(
+                            modifier = Modifier
+                                .onSizeChanged {
+                                    headerHeight = with(density) {
+                                        it.height.toDp()
+                                    }
+                                }
+                        ) {
                             ChatDetailHeader(
                                 chatUi = state.chatUi,
                                 isDetailPresent = isDetailPresent,
@@ -276,6 +326,20 @@ fun ChatDetailScreen(
                     }
                 }
             }
+            AnimatedVisibility(
+                visible = state.bannerState.isVisible,
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .padding(top = headerHeight + 16.dp),
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                if(state.bannerState.formattedDate != null) {
+                    DateChip(
+                        date = state.bannerState.formattedDate.asString()
+                    )
+                }
+            }
         }
     }
 }
@@ -307,6 +371,7 @@ private fun DynamicRoundedCornerColumn(
 private fun ChatDetailEmptyPreview() {
     ChirpTheme {
         ChatDetailScreen(
+            messageListState = rememberLazyListState(),
             state = ChatDetailState(),
             isDetailPresent = false,
             onAction = {},
@@ -321,6 +386,7 @@ private fun ChatDetailEmptyPreview() {
 private fun ChatDetailMessagesPreview() {
     ChirpTheme(darkTheme = true) {
         ChatDetailScreen(
+            messageListState = rememberLazyListState(),
             state = ChatDetailState(
                 messageTextFieldState = rememberTextFieldState(
                     initialText = "This is a new message!"
