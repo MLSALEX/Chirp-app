e0600cd5b4a0e260994f56f162451403251aa1af
MLSALEX <stiopovna@mail.ru>
2026-02-02 08:52:40 +0200

    Group messages by date

--- feature/chat/presentation/src/commonMain/kotlin/com/alexmls/chat/presentation/mappers/ChatMessageMappers.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/alexmls/chat/presentation/mappers/ChatMessageMappers.kt
@@ -3,11 +3,21 @@ package com.alexmls.chat.presentation.mappers
 import com.alexmls.chat.domain.models.MessageWithSender
 import com.alexmls.chat.presentation.model.MessageUi
 import com.alexmls.chat.presentation.util.DateUtils
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 fun List<MessageWithSender>.toUiList(localUserId: String): List<MessageUi> {
     return this
         .sortedByDescending { it.message.createdAt }
-        .map { it.toUi(localUserId) }
+        .groupBy {
+            it.message.createdAt.toLocalDateTime(TimeZone.currentSystemDefault()).date
+        }
+        .flatMap { (date, messages) ->
+            messages.map { it.toUi(localUserId) } + MessageUi.DateSeparator(
+                id = date.toString(),
+                date = DateUtils.formatDateSeparator(date)
+            )
+        }
 }
 
 fun MessageWithSender.toUi(
