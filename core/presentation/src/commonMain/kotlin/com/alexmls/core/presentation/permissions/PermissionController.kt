4746a4956c7805072d4517050108655dd46e3ecb
MLSALEX <stiopovna@mail.ru>
2026-02-06 09:04:57 +0200

    Request notification permission

---
+++ core/presentation/src/commonMain/kotlin/com/alexmls/core/presentation/permissions/PermissionController.kt
@@ -0,0 +1,5 @@
+package com.alexmls.core.presentation.permissions
+
+expect class PermissionController {
+    suspend fun requestPermission(permission: Permission): PermissionState
+}
---
+++ core/presentation/src/commonMain/kotlin/com/alexmls/core/presentation/permissions/rememberPermissionController.kt
@@ -0,0 +1,6 @@
+package com.alexmls.core.presentation.permissions
+
+import androidx.compose.runtime.Composable
+
+@Composable
+expect fun rememberPermissionController(): PermissionController
